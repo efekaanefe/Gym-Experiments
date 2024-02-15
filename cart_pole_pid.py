ed35eae5b3874fd0b8e3b83884ba766705b07af8
efekaanefe <efekaanefe1905@gmail.com>
2024-02-15 15:00:46 +0300

    how do i tune pid

--- cart_pole_pid.py
+++ cart_pole_pid.py
@@ -2,11 +2,7 @@ import gymnasium as gym
 import numpy as np
 
 
-env = gym.make("CartPole-v1", render_mode = "human")
 
-
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
 
 class PIDCartPoleAgent:
     def __init__(self, P, I, D):
@@ -22,19 +18,27 @@ class PIDCartPoleAgent:
         self.prev_error = error
         pid = self.p * error + self.i * self.integral + self.d * derivative
         action = 0 if pid < 0 else 1
+        action = np.round(sigmoid(pid)).astype(np.int16)
         return action
+    
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
 
 if __name__=="__main__":
-    P, I, D = 10,1,100
+    env = gym.make("CartPole-v1", render_mode = "human")
+    P, I, D = 10000, 0, 0
     agent = PIDCartPoleAgent(P, I, D)
     observation, info = env.reset()
     # original = observation[2]
     done = False
     while not done:
         env.render()
-        observation_error = observation[0] - observation[1] + observation[2] - 0
+        observation_error = observation[0] + observation[1] + observation[2] - 0 
+        print(observation_error)
         action = agent.choose_action(observation_error)
-        observation, reward, done, _ , _ = env.step(action)
+        observation, reward, done, truncated, info = env.step(action)
+        done = False
     env.close()
 
 
