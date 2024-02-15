d85eb82125d689d68dba626d8aee2de4671956fb
efekaanefe <efekaanefe1905@gmail.com>
2024-02-15 17:40:18 +0300

    turn to nonclass

--- cartpole_q_learning.py
+++ cartpole_q_learning.py
@@ -1,59 +1,44 @@
 import gymnasium as gym
 import numpy as np
-from sklearn.preprocessing import KBinsDiscretizer
 
-
-class QLearningAgent: # Quality Learning
-    def __init__(self, env, lr = 0.1, exploration_rate = 0.1, n_bins = (6,12)):
-        self.Q_table = np.zeros(n_bins + (env.action_space.n,))
-        print(f"Q table shape: {self.Q_table.shape}")
-        self.lr = lr
-        self.exploration_rate = exploration_rate
-
-    def new_Q_value(self, reward : float ,  new_state : tuple , discount_factor=1 ) -> float:
-        """Temperal diffrence for updating Q-value of state-action pair"""
-        future_optimal_value = np.max(self.Q_table[new_state])
-        learned_value = reward + discount_factor * future_optimal_value
-        return learned_value
-
-    def choose_action(self, state):
-        return np.argmax(self.Q_table[state])
 
 if __name__=="__main__":
     # https://www.youtube.com/watch?v=JNKvJEzuNsc
     # https://github.com/udacity/reinforcement-learning/blob/master/notebooks/Discretization.ipynb
     # https://www.youtube.com/watch?v=yMk_XtIEzH8&list=PLQVvvaa0QuDezJFIOU5wDdfy4e9vdnx-7
-    env = gym.make("CartPole-v1", render_mode = "human")
 
-    lower_bounds = [ env.observation_space.low[2], -np.radians(50) ]
-    upper_bounds = [ env.observation_space.high[2], np.radians(50) ]
-
-    def discretizer( _ , __ , angle, pole_velocity ) :
-        """Convert continues state intro a discrete state"""
-        est = KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy='uniform', subsample=200_000)
-        est.fit([lower_bounds, upper_bounds ])
-        return np.array(tuple(map(int,est.transform([[angle, pole_velocity]])[0])))
+    # env = gym.make("CartPole-v1", render_mode = "human")
+    env = gym.make("MountainCar-v0", render_mode = "human")
     
-    n_bins = (6,2) # shape: n_features
+    # only will look to 0th and 2nd observations
+    DISCRETE_OBS_SPACE_SIZE = [20] * len(env.observation_space.high)
+    discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OBS_SPACE_SIZE
+    print(discrete_os_win_size)
+    print(DISCRETE_OBS_SPACE_SIZE+ [env.action_space.n])
+    # every combination of observations and the action spaces
+    q_table = np.random.uniform(low=-2, high=2, size=(DISCRETE_OBS_SPACE_SIZE + [env.action_space.n]))
 
-    agent = QLearningAgent(env, n_bins = n_bins)
-    num_episodes = 1000
+    
+    def discretizer(position , _ , angle, __): # only two observations
+        return 0
+
+    num_episodes = 1
     for e in range(num_episodes):
         observation, info = env.reset()
         current_state = discretizer(*observation) 
         done = False; score = 0
         while not done:
-            action = agent.choose_action(current_state)
-            # if agent.exploration_rate > np.random.random():
-            #     action = env.action_space.sample() 
+            # action = choose_action(current_state)
+            # if exploration_rate(e) > np.random.random():
+            action = env.action_space.sample() 
 
             observation, reward, done, _, _ = env.step(action)
             new_state = discretizer(*observation)
 
             # update
-            learnt_value = agent.new_Q_value(reward , new_state )
-            old_value = agent.Q_table[current_state][action]
-            agent.Q_table[current_state][action] = (1-agent.lr)*old_value + agent.lr*learnt_value
+            # learnt_value = agent.new_Q_value(reward , new_state )
+            # old_value = agent.Q_table[current_state][action]
+            # agent.Q_table[current_state][action] = (1-agent.lr)*old_value + agent.lr*learnt_value
 
 
             current_state = new_state
