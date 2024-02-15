abc3587777cef87b157424af74beb79502ea0582
efekaanefe <efekaanefe1905@gmail.com>
2024-02-15 14:40:46 +0300

    pid almost

--- cart_pole.py
+++ cart_pole.py
@@ -4,48 +4,37 @@ import numpy as np
 
 env = gym.make("CartPole-v1", render_mode = "human")
 
-observation, info = env.reset()
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 class PIDCartPoleAgent:
-    def __init__(self, kp, ki, kd):
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.prev_error = 0
+    def __init__(self, P, I, D):
+        self.p = P
+        self.i = I
+        self.d = D
+        self.prev_error = 0 # error for all the observations, shape -> (4,)
         self.integral = 0
 
-    def choose_action(self, observation):
-        position, velocity, angle, angular_velocity = observation
-        target_position = np.array([0,0,0,0])
-        error = target_position - position
-
+    def choose_action(self, error):
         self.integral += error
-
         derivative = error - self.prev_error
         self.prev_error = error
-
-        pid = self.kp * error + self.ki * self.integral + self.kd * derivative
-
-        # if pid < 0:
-        #     action = 0  # Move left
-        # else:
-        #     action = 1  # Move right
-        action = sigmoid(pid)
-        action = np.round(action).astype(np.int32)
+        pid = self.p * error + self.i * self.integral + self.d * derivative
+        action = 0 if pid < 0 else 1
         return action
 
-P, I, D = [1/150, 1/950, 0.1, 0.01], [0.0005, 0.001, 0.01, 0.0001], [0.2, 0.0001, 0.5, 0.005]
-agent = PIDCartPoleAgent(P, I, D)
-
-observation, info = env.reset()
-done = False
-while not done:
-    env.render()
-    action = agent.choose_action(observation)
-    observation, reward, terminated, done , info = env.step(action)
-env.close()
+if __name__=="__main__":
+    P, I, D = 10,1,100
+    agent = PIDCartPoleAgent(P, I, D)
+    observation, info = env.reset()
+    # original = observation[2]
+    done = False
+    while not done:
+        env.render()
+        observation_error = observation[0] - observation[1] + observation[2] - 0
+        action = agent.choose_action(observation_error)
+        observation, reward, done, _ , _ = env.step(action)
+    env.close()
 
 
