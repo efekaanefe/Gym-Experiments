eea0b9be9d183ad8a18afb36ad549df49ae612c3
efekaanefe <efekaanefe1905@gmail.com>
2024-02-15 15:15:58 +0300

    a little increasing shift, but successfully makes it vertical

--- cartpole_pid.py
+++ cartpole_pid.py
@@ -2,8 +2,6 @@ import gymnasium as gym
 import numpy as np
 
 
-
-
 class PIDCartPoleAgent:
     def __init__(self, P, I, D):
         self.p = P
@@ -18,24 +16,24 @@ class PIDCartPoleAgent:
         self.prev_error = error
         pid = self.p * error + self.i * self.integral + self.d * derivative
         action = 0 if pid < 0 else 1
-        action = np.round(sigmoid(pid)).astype(np.int16)
+        # action = np.round(sigmoid(pid)).astype(np.int16)
         return action
     
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
-
 if __name__=="__main__":
     env = gym.make("CartPole-v1", render_mode = "human")
-    P, I, D = 10000, 0, 0
+    P, I, D = 20, 1, 80 # shifts a little from middle but works
+    # P, I, D = 20, 1, 100
+
     agent = PIDCartPoleAgent(P, I, D)
     observation, info = env.reset()
     # original = observation[2]
     done = False
     while not done:
         env.render()
-        observation_error = observation[0] + observation[1] + observation[2] - 0 
-        print(observation_error)
+        observation_error = observation[0] + np.rad2deg(observation[2]) - 0 - observation[1] 
         action = agent.choose_action(observation_error)
         observation, reward, done, truncated, info = env.step(action)
         done = False
