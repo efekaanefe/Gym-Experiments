45170d927bd5d154482181e949b7516a00552a4b
efekaanefe <efekaanefe1905@gmail.com>
2024-02-16 10:39:53 +0300

    q table

--- cartpole_q_learning.py
+++ cartpole_q_learning.py
@@ -9,31 +9,43 @@ if __name__=="__main__":
 
     env = gym.make("CartPole-v1", render_mode = "human")
 
-    # only working with obs[0] and obs[2], since others can be +-inf, not good for q-learning
+    # only working with obs[0] and obs[2], since others can have value of +-inf, not good for q-learning
     max_obs_values = env.observation_space.high
     min_obs_values = env.observation_space.low
     max_obs_values = np.array([max_obs_values[0], np.rad2deg(max_obs_values[2])])
     min_obs_values = np.array([min_obs_values[0], np.rad2deg(min_obs_values[2])])
 
+    ## DISCRETIZING
     DISCRETE_OBS_SPACE_SIZE = [20]* len(max_obs_values) # these are the what the max values will corresponds to
-    # DISCRETE_OBS_SPACE_SIZE = [20, 30] 
     discrete_obs_space_step_size = (max_obs_values - min_obs_values) / DISCRETE_OBS_SPACE_SIZE
 
+    def discretizer(obs):
+        obs = np.array([obs[0], obs[2]])
+        discrete_obs = (obs - min_obs_values)/discrete_obs_space_step_size
+        return discrete_obs.astype(np.int16)
+    
+    ## CONSTANTS, these may be adjusted based on episode number
+    LEARNING_RATE = 0.1
+    DISCOUNT = 0.95
+    EXPLORATION_RATE = 0.2
+
+    ## Q-table
+    q_table = np.random.uniform(min=-2,max=2, size=(DISCRETE_OBS_SPACE_SIZE + [env.action_space.n]))
 
     num_episodes = 1
     for e in range(num_episodes):
         observation, info = env.reset()
-        current_state = discretizer(*observation) 
+        current_obs = discretizer(observation) 
         done = False; score = 0
         while not done:
-            # action = choose_action(current_state)
-            # if exploration_rate(e) > np.random.random():
-            action = env.action_space.sample() 
+            action = np.argmax(q_table[current_obs[0]][current_obs[1]])
+            if EXPLORATION_RATE > np.random.random():
+                action = env.action_space.sample() 
 
             observation, reward, done, _, _ = env.step(action)
-            new_state = discretizer(*observation)
+            new_state = discretizer(observation)
 
-            # update
+            ## TRAINING
             # learnt_value = agent.new_Q_value(reward , new_state )
             # old_value = agent.Q_table[current_state][action]
             # agent.Q_table[current_state][action] = (1-agent.lr)*old_value + agent.lr*learnt_value
